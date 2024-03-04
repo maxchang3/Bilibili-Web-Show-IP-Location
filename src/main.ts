c24b9194b335048f3ceb9e1c8b919d4932f3ffae
MaxChang3 <maxcfoo@gmail.com>
2024-03-04 17:34:05 +0800

    style: format comments

--- src/main.ts
+++ src/main.ts
@@ -8,17 +8,15 @@ import { isElementLoaded, Router } from "@/utils/"
 const router = new Router()
 
 router.serve([
-    // 视频
-    "https://www.bilibili.com/video/",
-    // 新列表 
-    "https://www.bilibili.com/list/",
-    // 新版单独动态页
-    "https://www.bilibili.com/opus/",
-    // 课程页
-    "https://www.bilibili.com/cheese/play/"
+    /** 视频  */ "https://www.bilibili.com/video/",
+    /** 新列表  */ "https://www.bilibili.com/list/",
+    /** 新版单独动态页 */ "https://www.bilibili.com/opus/",
+    /** 课程页 */ "https://www.bilibili.com/cheese/play/"
 ], observeAndInjectComments)
 
-// 番剧
+/**
+ * 番剧播放页
+ */
 router.serve("https://www.bilibili.com/bangumi/play/", () => {
     const isNewBangumi = !!document.querySelector("meta[name=next-head-count]")
     if (isNewBangumi) {
@@ -28,13 +26,19 @@ router.serve("https://www.bilibili.com/bangumi/play/", () => {
     }
 })
 
-// 话题页
+/**
+ * 话题页
+ */
 router.serve("https://www.bilibili.com/v/topic/detail/", () => serveNewComments(".list-view"))
 
-// 个人空间动态页
+/** 
+ * 个人空间动态页
+ */
 router.serve("https://space.bilibili.com/", () => serveNewComments(".bili-dyn-list__items"), { endsWith: "dynamic" })
 
-// 个人空间首页
+/** 
+ * 个人空间首页
+ */
 router.serve("https://space.bilibili.com/", async () => {
     const dynamicTab = await isElementLoaded('.n-dynamic')
     dynamicTab.addEventListener('click', () => {
@@ -42,7 +46,9 @@ router.serve("https://space.bilibili.com/", async () => {
     }, { once: true })
 })
 
-// 动态主页
+/** 
+ * 动态主页
+ */
 router.serve("https://t.bilibili.com/", async () => {
     const dynHome = await isElementLoaded('.bili-dyn-home--member')
     const isNewDyn = (dynHome.querySelector('.bili-dyn-sidebar__btn') as HTMLElement | undefined)?.innerText.startsWith("新版反馈")
@@ -53,7 +59,9 @@ router.serve("https://t.bilibili.com/", async () => {
     }
 }, { endsWith: '/' })
 
-// 单独动态页
+/** 
+ * 单独动态页
+ */
 router.serve("https://t.bilibili.com/", async () => {
     const dynItem = await isElementLoaded('.bili-dyn-item')
     const isNewDyn = !!dynItem.querySelector('.bili-comment-container')
@@ -65,10 +73,14 @@ router.serve("https://t.bilibili.com/", async () => {
     }
 })
 
-// 拜年祭
+/** 
+ * 拜年祭
+ */
 router.serve("https://www.bilibili.com/festival/", hookBBComment)
 
-// 小黑屋
+/** 
+ * 小黑屋
+ */
 router.serve("https://www.bilibili.com/blackroom/ban/", () => hookBBComment({ blackroom: true }))
 
 router.match(location.href)
