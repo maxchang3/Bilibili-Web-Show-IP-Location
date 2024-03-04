f83c0464d94c87a6d88b4d089de8b6e8ea9a04e7
MaxChang3 <maxcfoo@gmail.com>
2024-03-04 21:23:40 +0800

    refactor: router use array instead of map

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -2,16 +2,16 @@ interface RouterOption {
     endsWith?: string
 }
 export class Router {
-    routes: Map<[string, RouterOption], Function> = new Map()
+    routes: [route: string, routeFunction: Function, option: RouterOption][] = []
     serve(route: string | string[], routeFunction: Function, option: RouterOption = {}) {
         if (Array.isArray(route)) {
-            route.forEach(r => this.routes.set([r, option], routeFunction))
+            route.forEach(r => this.routes.push([r, routeFunction, option]))
             return
         }
-        this.routes.set([route, option], routeFunction)
+        this.routes.push([route, routeFunction, option])
     }
     match(url: string) {
-        for (let [[route, option], routeFunction] of this.routes.entries()) {
+        for (let [route, routeFunction, option] of this.routes) {
             if (!url.startsWith(route)) continue
             if (option.endsWith && !url.endsWith(option.endsWith)) continue
             routeFunction()
