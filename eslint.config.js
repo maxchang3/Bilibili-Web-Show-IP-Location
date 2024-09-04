c42194cf5932c0c9d36d99dabd70f7b0620c813f
MaxChang3 <maxcfoo@gmail.com>
2024-09-04 18:55:40 +0800

    style: update brace-style rule to allow single line

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
         'jsonc/indent': ['error', 2],
         'jsdoc/require-returns-description': 'off',
         'style/max-statements-per-line': 'off',
-        'style/brace-style': ['error', '1tbs'],
+        'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
         'ts/method-signature-style': ['error', 'method'],
     },
 })
